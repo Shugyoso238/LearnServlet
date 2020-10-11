521061f5d9867330179092af312f0aa94b867e88
Syugyoso238 <fumiya.mizoe@gmail.com>
2020-10-11 19:10:05 +0900

    chapter11 サーブレットのライフサイクル追加

---
+++ book/src/chapter11/LifeCycle.java
@@ -0,0 +1,57 @@
+package chapter11;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LifeCycle
+ */
+@WebServlet("/chapter11/life-cycle")
+public class LifeCycle extends HttpServlet 
+{
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("init");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("destroy");
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("service");
+		super.service(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		System.out.println("doGet");
+	}
+
+}
